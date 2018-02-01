3d8d6256ea49df43172e7accda25310ef51f3ec1
karsa <wanghongtao114@gmail.com>
2018-02-01 10:21:26 +0800

    Initial Commit

---
+++ AppObserver/AppObserver.h
@@ -0,0 +1,19 @@
+//
+//  AppObserver.h
+//  AppObserver
+//
+//  Created by karsa on 2018/2/1.
+//  Copyright © 2018年 karsa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppObserver.
+FOUNDATION_EXPORT double AppObserverVersionNumber;
+
+//! Project version string for AppObserver.
+FOUNDATION_EXPORT const unsigned char AppObserverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppObserver/PublicHeader.h>
+
+
